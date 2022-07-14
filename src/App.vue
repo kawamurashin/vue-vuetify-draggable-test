10f3d54d855c76567f6865c09f8bd5d283d51a47
川村　森 <kawamura@mztm.jp>
2022-07-14 14:53:47 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <draggable
+        v-model="myArray"
+        group="people"
+        @start="drag=true"
+        @end="drag=false"
+        item-key="id"
+        v-bind="dragOptions"
+        handle=".handle">
+      <template #item="{element}">
+        <div class="list-group-item">
+          <span class="handle">
+            <v-icon
+                large
+                color="black"
+            style="margin: 10px">
+              mdi-drag-horizontal-variant
+            </v-icon>
+          </span>
+          <span class="text">{{ element.name }} </span>
+        </div>
+      </template>
+    </draggable>
+  </div>
 </template>
-
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import {defineComponent} from 'vue'
+import draggable from 'vuedraggable'
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
-});
+    draggable
+  },
+  data() {
+    return {
+      myArray: [
+        {name: "John", id: 0},
+        {name: "Joao", id: 1},
+        {name: "Jean", id: 2},
+        {name: "004", id: 3},
+        {name: "4", id: 4},
+        {name: "Jean5", id: 5}
+      ],
+      drag: false
+    }
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
+  },
+})
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.ghost {
+  opacity: 0.75;
+  background: #c8ebfb;
+}
+.list-group-item {
+  padding: 5px;
+  border-bottom: solid #000000 1px;
+}
+.handle {
+  cursor: move;
 }
 </style>
